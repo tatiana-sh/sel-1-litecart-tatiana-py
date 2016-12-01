72129daad928c2d0240cb0c2744f4944e23b2045
tatiana-sh <shabskaya@gmail.com>
2016-12-01 14:27:12 +0200

    fixed test to check there is one sticker instead of there is no second sticker

--- tests/test_litecart_product_list_view.py
+++ tests/test_litecart_product_list_view.py
@@ -10,21 +10,9 @@ class TestLitecartProductListView(TestBase):
         request.cls.driver.get(MainPageLocators.BASE_URL)
 
     def test_each_product_in_list_should_have_no_more_stickers_than_allowed(self, opened_product_list_view):
-        second_stickers_list = self.driver.find_elements(*MainPageLocators.SECOND_PRODUCT_STICKER)
-        assert len(second_stickers_list) == 0
+        products_list_count = len(self.driver.find_elements(*MainPageLocators.PRODUCT))
+        for i in range(0, products_list_count):
+            current_product = self.driver.find_elements(*MainPageLocators.PRODUCT)[i]
 
-        categories_list_count = len(
-            self.driver.find_elements(*MainPageLocators.NAVIGATION_MENU_ITEM))
-        for i in range(0, categories_list_count):
-            self.driver.find_elements(*MainPageLocators.NAVIGATION_MENU_ITEM)[i].click()
-
-            second_stickers_list = self.driver.find_elements(*MainPageLocators.SECOND_PRODUCT_STICKER)
-            assert len(second_stickers_list) == 0
-
-            subcategories_list_count = len(
-                self.driver.find_elements(*MainPageLocators.NAVIGATION_MENU_ITEM_SUBNODE))
-            for j in range(0, subcategories_list_count):
-                self.driver.find_elements(*MainPageLocators.NAVIGATION_MENU_ITEM_SUBNODE)[j].click()
-
-                second_stickers_list = self.driver.find_elements(*MainPageLocators.SECOND_PRODUCT_STICKER)
-                assert len(second_stickers_list) == 0
+            stickers_list = current_product.find_elements(*MainPageLocators.PRODUCT_STICKER)
+            assert len(stickers_list) == 1
