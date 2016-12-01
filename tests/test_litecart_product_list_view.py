b4f3990a87174a758166881d639a65b6d8d5fe55
tatiana-sh <shabskaya@gmail.com>
2016-12-01 14:04:07 +0200

    added base test case class and ui map for tests

--- tests/test_litecart_product_list_view.py
+++ tests/test_litecart_product_list_view.py
@@ -1,36 +1,30 @@
 import pytest
-from selenium import webdriver
+
+from pages.locators import MainPageLocators
+from tests.test_base import TestBase
 
 
-class TestLitecartProductListView:
-    @pytest.fixture
-    def driver(self, request):
-        wd = webdriver.Chrome()
-        wd.implicitly_wait(3)
-
-        request.addfinalizer(wd.quit)
-        request.cls.driver = wd
-
+class TestLitecartProductListView(TestBase):
     @pytest.fixture()
     def opened_product_list_view(self, request, driver):
-        request.cls.driver.get("http://localhost/litecart/en/")
+        request.cls.driver.get(MainPageLocators.BASE_URL)
 
     def test_each_product_in_list_should_have_no_more_stickers_than_allowed(self, opened_product_list_view):
-        second_stickers_list = self.driver.find_elements_by_css_selector(".product .sticker:nth-of-type(2)")
+        second_stickers_list = self.driver.find_elements(*MainPageLocators.SECOND_PRODUCT_STICKER)
         assert len(second_stickers_list) == 0
 
         categories_list_count = len(
-            self.driver.find_elements_by_css_selector("#box-category-tree .content > ul > li > a"))
+            self.driver.find_elements(*MainPageLocators.NAVIGATION_MENU_ITEM))
         for i in range(0, categories_list_count):
-            self.driver.find_elements_by_css_selector("#box-category-tree .content > ul > li > a")[i].click()
+            self.driver.find_elements(*MainPageLocators.NAVIGATION_MENU_ITEM)[i].click()
 
-            second_stickers_list = self.driver.find_elements_by_css_selector(".product .sticker:nth-of-type(2)")
+            second_stickers_list = self.driver.find_elements(*MainPageLocators.SECOND_PRODUCT_STICKER)
             assert len(second_stickers_list) == 0
 
             subcategories_list_count = len(
-                self.driver.find_elements_by_css_selector("#box-category-tree .content > ul > li li > a"))
+                self.driver.find_elements(*MainPageLocators.NAVIGATION_MENU_ITEM_SUBNODE))
             for j in range(0, subcategories_list_count):
-                self.driver.find_elements_by_css_selector("#box-category-tree .content > ul > li li > a")[j].click()
+                self.driver.find_elements(*MainPageLocators.NAVIGATION_MENU_ITEM_SUBNODE)[j].click()
 
-                second_stickers_list = self.driver.find_elements_by_css_selector(".product .sticker:nth-of-type(2)")
+                second_stickers_list = self.driver.find_elements(*MainPageLocators.SECOND_PRODUCT_STICKER)
                 assert len(second_stickers_list) == 0
