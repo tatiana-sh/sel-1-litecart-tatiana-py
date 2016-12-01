e2640528add4c6ffbde8551d2cc7dd03a363da2d
tatiana-sh <shabskaya@gmail.com>
2016-12-01 12:45:02 +0200

    updated fixtures to log in and go to required page

--- tests/test_litecart_admin_navigation.py
+++ tests/test_litecart_admin_navigation.py
@@ -6,32 +6,34 @@ class TestLitecartAdminNavigation:
     @pytest.fixture
     def driver(self, request):
         wd = webdriver.Chrome()
-        wd.implicitly_wait(1)
+        wd.implicitly_wait(2)
 
-        wd.get("http://localhost/litecart/admin/login.php")
+        request.addfinalizer(wd.quit)
+        request.cls.driver = wd
 
-        username_input = wd.find_element_by_name("username")
-        password_input = wd.find_element_by_name("password")
-        login_btn = wd.find_element_by_name("login")
+    @pytest.fixture()
+    def logged_in_admin(self, request, driver):
+        request.cls.driver.get("http://localhost/litecart/admin/login.php")
+
+        username_input = request.cls.driver.find_element_by_name("username")
+        password_input = request.cls.driver.find_element_by_name("password")
+        login_btn = request.cls.driver.find_element_by_name("login")
 
         username_input.send_keys("admin")
         password_input.send_keys("admin")
         login_btn.click()
 
-        request.addfinalizer(wd.quit)
-        return wd
-
-    def test_admin_should_be_able_to_navigate_and_see_headers_on_each_page(self, driver):
-        navigation_node_count = len(driver.find_elements_by_css_selector("#box-apps-menu #app-"))
+    def test_admin_should_be_able_to_navigate_and_see_headers_on_each_page(self, logged_in_admin):
+        navigation_node_count = len(self.driver.find_elements_by_css_selector("#box-apps-menu #app-"))
         for i in range(0, navigation_node_count):
-            driver.find_elements_by_css_selector("#box-apps-menu #app-")[i].click()
+            self.driver.find_elements_by_css_selector("#box-apps-menu #app-")[i].click()
 
-            page_header = driver.find_element_by_css_selector("h1")
+            page_header = self.driver.find_element_by_css_selector("h1")
             assert page_header.is_displayed and page_header.text != ""
 
-            navigation_subnode_count = len(driver.find_elements_by_css_selector("#box-apps-menu #app- li"))
+            navigation_subnode_count = len(self.driver.find_elements_by_css_selector("#box-apps-menu #app- li"))
             for j in range(0, navigation_subnode_count):
-                driver.find_elements_by_css_selector("#box-apps-menu #app- li")[j].click()
+                self.driver.find_elements_by_css_selector("#box-apps-menu #app- li")[j].click()
 
-                page_header = driver.find_element_by_css_selector("h1")
+                page_header = self.driver.find_element_by_css_selector("h1")
                 assert page_header.is_displayed and page_header.text != ""
--- tests/test_litecart_product_list_view.py
+++ tests/test_litecart_product_list_view.py
@@ -7,26 +7,30 @@ class TestLitecartProductListView:
     def driver(self, request):
         wd = webdriver.Chrome()
         wd.implicitly_wait(3)
+
         request.addfinalizer(wd.quit)
-        return wd
+        request.cls.driver = wd
 
-    def test_each_product_in_list_should_have_no_more_stickers_than_allowed(self, driver):
-        driver.get("http://localhost/litecart/en/")
+    @pytest.fixture()
+    def opened_product_list_view(self, request, driver):
+        request.cls.driver.get("http://localhost/litecart/en/")
 
-        second_stickers_list = driver.find_elements_by_css_selector(".product .sticker:nth-of-type(2)")
-        assert len(second_stickers_list) is 0
+    def test_each_product_in_list_should_have_no_more_stickers_than_allowed(self, opened_product_list_view):
+        second_stickers_list = self.driver.find_elements_by_css_selector(".product .sticker:nth-of-type(2)")
+        assert len(second_stickers_list) == 0
 
-        categories_list_count = len(driver.find_elements_by_css_selector("#box-category-tree .content > ul > li > a"))
+        categories_list_count = len(
+            self.driver.find_elements_by_css_selector("#box-category-tree .content > ul > li > a"))
         for i in range(0, categories_list_count):
-            driver.find_elements_by_css_selector("#box-category-tree .content > ul > li > a")[i].click()
+            self.driver.find_elements_by_css_selector("#box-category-tree .content > ul > li > a")[i].click()
 
-            second_stickers_list = driver.find_elements_by_css_selector(".product .sticker:nth-of-type(2)")
-            assert len(second_stickers_list) is 0
+            second_stickers_list = self.driver.find_elements_by_css_selector(".product .sticker:nth-of-type(2)")
+            assert len(second_stickers_list) == 0
 
             subcategories_list_count = len(
-                driver.find_elements_by_css_selector("#box-category-tree .content > ul > li li > a"))
+                self.driver.find_elements_by_css_selector("#box-category-tree .content > ul > li li > a"))
             for j in range(0, subcategories_list_count):
-                driver.find_elements_by_css_selector("#box-category-tree .content > ul > li li > a")[j].click()
+                self.driver.find_elements_by_css_selector("#box-category-tree .content > ul > li li > a")[j].click()
 
-                second_stickers_list = driver.find_elements_by_css_selector(".product .sticker:nth-of-type(2)")
-                assert len(second_stickers_list) is 0
+                second_stickers_list = self.driver.find_elements_by_css_selector(".product .sticker:nth-of-type(2)")
+                assert len(second_stickers_list) == 0
